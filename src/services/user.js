7f8f71605c377aa537388442f48fb11d6c91c803
jehfkemsy <jehf.denezaire@gmail.com>
2020-10-19 11:29:22 -0400

    added create user

--- src/services/user.js
+++ src/services/user.js
@@ -1,18 +1,25 @@
+import { firestore } from "../connections/firebase-admin.js";
+
 export default class UserService {
   constructor() {}
 
-  create = async (ctx) => {
+  create = async ctx => {
     const { email, id } = ctx.state.user;
     const {
       firstName,
       lastName,
       photo = "https://randomuser.me/api/portraits/lego/0.jpg",
+      company = ""
     } = ctx.request.body;
 
-    ctx.body = { id };
+    const data = await firestore
+      .collection("users")
+      .add({ id, firstName, lastName, photo, company, email });
+
+    ctx.body = { success: true, data: { id, firstName, lastName, photo, company, email } };
   };
 
-  read = async (ctx) => {
+  read = async ctx => {
     const { id } = ctx.params;
 
     const user = {};
